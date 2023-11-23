6e88a4ab8fc9b7cba10eb97819d38f5dd14408af
Christian Isaman <98602195+thrivingproject@users.noreply.github.com>
2023-11-23 18:23:05 -0400

    Convert script to class

--- polynomials/multiply_polynomials.py
+++ polynomials/multiply_polynomials.py
@@ -1,32 +1,28 @@
-poly1 = [1, 9]
-poly2 = [1, 2, 0]
-variable = "x"
+class Polynomial_Multiplier:
+    def __init__(self, poly_1, poly_2, variable='x'):
+        self.poly_1 = poly_1
+        self.poly_2 = poly_2
+        self.variable = variable
+        self.result_coefficients = [0] * (len(self.poly_1) + len(self.poly_2) - 1)
+        self.result_terms = []
+
+    def multiply(self):
+        for i in range(len(self.poly_1)):
+            for j in range(len(self.poly_2)):
+                self.result_coefficients[i + j] += self.poly_1[i] * self.poly_2[j]
+        return self
+
+    def print_result(self):
+        for i in range(len(self.result_coefficients)):
+            if self.result_coefficients[i] != 0:
+                degree = len(self.result_coefficients) - i - 1
+                if degree == 0:
+                    self.result_terms.append(str(self.result_coefficients[i]))
+                elif degree == 1:
+                    self.result_terms.append(f"{self.result_coefficients[i]}{self.variable}")
+                else:
+                    self.result_terms.append(f"{self.result_coefficients[i]}{self.variable}^{degree}")
+        print(" + ".join(self.result_terms))
 
 
-def multiply_polynomials(poly1, poly2, variable="x"):
-    # Initialize the result polynomial (using list replication)
-    result = [0] * (len(poly1) + len(poly2) - 1)
-
-    # Multiply each term of the first polynomial
-    # with each term of the second polynomial
-    for i in range(len(poly1)):
-        for j in range(len(poly2)):
-            result[i + j] += poly1[i] * poly2[j]
-
-    # Convert to string
-    terms = []
-    for i in range(len(result)):
-        if result[i] != 0:
-            # Get the degree of the term
-            degree = len(result) - i - 1
-            # Format the term
-            if degree == 0:
-                terms.append(str(result[i]))
-            elif degree == 1:
-                terms.append(f"{result[i]}{variable}")
-            else:
-                terms.append(f"{result[i]}{variable}^{degree}")
-    return " + ".join(terms)
-
-
-print("Resultant Polynomial: ", multiply_polynomials(poly1, poly2, variable))
+Polynomial_Multiplier([3, 2], [3, 0, 4, 0, 5]).multiply().print_result()
